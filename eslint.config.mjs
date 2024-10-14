1f7ea62ae5e5dd83a9affc604adaad150f6d9f34
Jack Maclennan <jackwmaclennan@gmail.com>
2024-10-15 11:50:19 +1300

    add prettier and upgrade eslint - config prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,83 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import jsxA11Y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default ts.config(
+  {
+    ignores: [
+      '.next',
+      'node_modules',
+      '.gitignore',
+      '.prettierrc.json',
+      'eslint.config.mjs',
+      'next.config.mjs',
+      'postcss.config.mjs',
+      'tailwind.config.js'
+    ]
+  },
+  ...compat.extends(
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ),
+  js.configs.recommended,
+  // add `typescript-eslint` flat config simply
+  // if you would like use more another configuration,
+  // see the section: https://typescript-eslint.io/getting-started#details
+  ...ts.configs.recommended,
+  {
+    plugins: {
+      'jsx-a11y': jsxA11Y,
+      prettier: prettier,
+      react: react,
+      'react-hooks': reactHooks
+    },
+
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: './tsconfig.json'
+      }
+    },
+
+    rules: {
+      'react/jsx-props-no-spreading': 1,
+      'react/require-default-props': 0,
+      'react/react-in-jsx-scope': 0,
+      '@typescript-eslint/comma-dangle': 0,
+
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          tabWidth: 2,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true
+        }
+      ],
+
+      'jsx-a11y/click-events-have-key-events': 1,
+      'jsx-a11y/no-static-element-interactions': 1,
+      'jsx-a11y/no-noninteractive-tabindex': 1,
+      'jsx-a11y/no-noninteractive-element-interactions': 1
+    }
+  }
+);
